4a4b2d43442351a158cd0e610e63badefec9a121
Ezer <ezer.fernandes@yahoo.com.br>
2024-01-18 07:19:10 -0300

    Delete 2015/p001/target directory

--- 2015/p001/target/debug/deps/p001-0e6cdec173212675.d
+++
@@ -1,5 +0,0 @@
-/home/ezersilva/repos/github-pub/rust-advents-of-code/2015/p001/target/debug/deps/p001-0e6cdec173212675.rmeta: src/main.rs
-
-/home/ezersilva/repos/github-pub/rust-advents-of-code/2015/p001/target/debug/deps/p001-0e6cdec173212675.d: src/main.rs
-
-src/main.rs:
