5fef815d9401e96b316596413ef5330546fcc385
Ezer Silva <ezer.silva@clickbank.com>
2024-01-18 07:17:36 -0300

    initial commit

---
+++ 2015/p001/target/debug/deps/p001-807372a4044328b9.d
@@ -0,0 +1,5 @@
+/home/ezersilva/repos/github-pub/rust-advents-of-code/2015/p001/target/debug/deps/p001-807372a4044328b9.rmeta: src/main.rs
+
+/home/ezersilva/repos/github-pub/rust-advents-of-code/2015/p001/target/debug/deps/p001-807372a4044328b9.d: src/main.rs
+
+src/main.rs:
